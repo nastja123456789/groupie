0e913240516edf69aa209a063bb62b5c2f568b2e
Алешина Анастасия Игоревна <aialeshina@edu.hse.ru>
2023-01-24 09:40:26 +0300

    mappers/MovieMapper.kt

--- app/src/main/java/ru/androidschool/intensiv/data/mappers/MovieMapper.kt
+++ app/src/main/java/ru/androidschool/intensiv/data/mappers/MovieMapper.kt
@@ -1,13 +1,31 @@
 package ru.androidschool.intensiv.data.mappers
 
+import ru.androidschool.intensiv.data.dto.MovieModelDB
 import ru.androidschool.intensiv.data.vo.MovieModel
 import ru.androidschool.intensiv.data.vo.MoviesResponse
 import ru.androidschool.intensiv.data.vo.TVModel
 import ru.androidschool.intensiv.data.vo.TVResponse
 import ru.androidschool.intensiv.ui.tvshows.Movie
+import ru.androidschool.intensiv.data.vo.Movie as movie
 
 object MovieMapper {
     fun toValueObject(list: TVResponse): List<TVModel> {
         return list.results
     }
+    fun fromMovieDBtoMovie(movieModelDB: MovieModelDB): movie = movie(
+        title = movieModelDB.title,
+        voteAverage = 0.0
+    )
+    fun fromMovieDBtoMovieModel(movieModelDB: MovieModelDB):MovieModel = MovieModel(
+        title = movieModelDB.title,
+        voteAverage = 0.0,
+        overview = "",
+        releaseDate = null
+    )
+    fun fromMovieToMovieDB(movie: movie):MovieModelDB = MovieModelDB(
+        title = movie.title.toString()
+    )
+    fun fromMovieModelToMovieDB(movieModel: MovieModel): MovieModelDB = MovieModelDB(
+        title = movieModel.title.toString()
+    )
 }
